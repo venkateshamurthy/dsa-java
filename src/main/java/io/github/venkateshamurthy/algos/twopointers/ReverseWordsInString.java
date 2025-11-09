03926056fea13d777049e565e4b32eb8fd89a9cb
venkateshamurthyy <venkateshamurthyts@gmail.com>
2025-11-09 14:10:05 +0530

    Updated the right pointer max length to upto as arr.length

--- src/main/java/io/github/venkateshamurthy/algos/twopointers/ReverseWordsInString.java
+++ src/main/java/io/github/venkateshamurthy/algos/twopointers/ReverseWordsInString.java
@@ -6,12 +6,11 @@ import static io.github.venkateshamurthy.Utils.swapRange;
 public class ReverseWordsInString {
     public static String reverse(String  sentence){
         char[] arr = sentence.toCharArray();
-
         swapRange(arr, 0, arr.length - 1);
         System.out.println("Reversed totally  :-->"+new String(arr));
 
         int left = 0, right = 0;
-        while( right < arr.length - 1 ) {
+        while( right < arr.length ) {
             // Find and increase right till white space comes or till end
             while(right < arr.length && arr[right] != ' ') right++;
 
@@ -29,7 +28,7 @@ public class ReverseWordsInString {
     }
 
     public static void main(String[] args) {
-        String sentence = "";
+        String sentence = "I am   for  very           good reasons very tenacious";
         System.out.println("Forward words only:-->"+sentence);
         System.out.println("Reverse words only:-->"+reverse(sentence));
     }
